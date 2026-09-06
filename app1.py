2604926bae33548cf6fb759a343a5250d39a1616
MaiTran <233404050212@ldxh.edu.vn>
2026-09-06 15:55:12 +0700

    feat: them API counter va API info

--- app1.py
+++ app1.py
@@ -1,6 +1,27 @@
+import os # them dong nay len dau file, truoc "from flask import Flask"
 from flask import Flask
 import datetime, platform
 app = Flask(__name__)
+
+visit_count = 0 # Bien dem luu trong RAM cua container
+
+@app.route("/api/counter")
+def counter():
+    global visit_count
+    visit_count += 1
+    return {
+        "so_lan_truy_cap": visit_count,
+        "ghi_chu": "So nay se MAT khi container khoi dong lai!"
+    }
+
+@app.route("/api/info")
+def info():
+    ten_sinh_vien = os.environ.get("VoPhamMaiTran", "Chua dat bien moi truong")
+    return {
+        "sinh_vien": ten_sinh_vien,
+    "nguon_du_lieu": "Environment Variable tren Render, KHONG hardcode trong code"
+    }
+
 @app.route("/")
 def home():
     return f"""
